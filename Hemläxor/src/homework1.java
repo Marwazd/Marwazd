bb95cb853de536679842ca5561175ead651db536
Marwazd <marwa.zeitouni@elev.ga.ntig.se>
2019-10-03 10:33:39 +0200

    vecka37

--- Hemläxor/src/homework1.java
+++ Hemläxor/src/homework1.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class homework1 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-	}
-
+		Scanner Hemläxor = new Scanner(System.in);
+		String name = Hemläxor.nextLine();
+		
+			System.out.print(10);
+			System.out.print(9);
+			System.out.print(8);
+			System.out.print(7);
+			System.out.print(6);
+			System.out.print(5);
+			System.out.print(4);
+			System.out.print(3);
+			System.out.print(2);
+			System.out.print(1);
+			System.out.print(0);
+		}
 }
+	
