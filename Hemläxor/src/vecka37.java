6198514d0d84dd13ad3d0a51588a1105895df888
Marwazd <marwa.zeitouni@elev.ga.ntig.se>
2019-10-03 19:57:35 +0200

    vecka37

--- Hemläxor/src/vecka37.java
+++ Hemläxor/src/vecka37.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
 public class vecka37 {
+	 public static void main(String[] args) {
+		 Scanner input = new Scanner(System.in);
+	
+		 System.out.print("Namn:");
+		 String name = input.next();
+		 System.out.print("Ålder:");
+		 String age = input.next();
+		 System.out.print("Adress:");
+		 
+		 
+		
+		 	
+		 	
+	 }
+	
 
-}
+} 
