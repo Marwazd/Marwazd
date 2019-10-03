50a0509e35c618dc197547d93def0732d1936532
Marwazd <marwa.zeitouni@elev.ga.ntig.se>
2019-10-03 20:02:33 +0200

    vecka37

--- Hemläxor/src/vecka37.java
+++ Hemläxor/src/vecka37.java
@@ -4,11 +4,15 @@ public class vecka37 {
 	 public static void main(String[] args) {
 		 Scanner input = new Scanner(System.in);
 	
-		 System.out.print("Namn:");
-		 String name = input.next();
-		 System.out.print("Ålder:");
-		 String age = input.next();
-		 System.out.print("Adress:");
+		 System.out.print("Namn: ");
+		 String namn = input.next();
+		 System.out.print("Ålder: ");
+		 String ålder = input.next();
+		 System.out.print("Adress: ");
+		 String adress = input.next();
+		 System.out.print("Telefon: ");
+		 String telefon = input.next();
+		 
 		 
 		 
 		
