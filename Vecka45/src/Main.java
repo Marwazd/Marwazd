f28915c240273ba8ade9007a10f249b5d1d86262
Marwazd <marwa.zeitouni@elev.ga.ntig.se>
2019-11-21 10:23:40 +0100

    method 2

---
+++ Vecka45/src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	static double g_swe = 9.82;
+	static double G = 6.6740831 * 10 * Math.exp(-11);
+	static double R = 8.314462175;
+	static int p_0 = 1000;
+	static int c = 299792458;
+	
+	
+	public static void main(String[] args) {
+
+		System.out.print (fahrenheitToCelsius(50));
+		System.out.print (kelvinToCelsius(0));
+		
+	}
+	
+	public static double fahrenheitToCelsius(double fahrenheit) {
+		double Celsius = (fahrenheit-32)* 5 / 9;
+		return Celsius;
+	}
+	
+	public static double kelvinToCelsius(double kelvin) {
+		double Celsius = kelvin - 273.15;
+		return Celsius;
+	} 
+}
