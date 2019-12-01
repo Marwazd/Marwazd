dfa6bf8e462da06ba4f0492c4e98ef6351639c9d
Marwazd <marwa.zeitouni@elev.ga.ntig.se>
2019-12-01 21:48:44 +0100

    2 gaser adderat!

--- Vecka45/src/GasTable.java
+++ Vecka45/src/GasTable.java
@@ -3,7 +3,9 @@ public enum GasTable{
 
 	AIR(1.29, 1.01), 
 	O2(1.43, 0.92), 
-	OXYGEN(1.43, 0.92)
+	OXYGEN(1.43, 0.92),
+	H /*Helium*/ (0.1785, 5.2E3),
+	Ne /*Neon*/ (0.9002, 1031),
 	;
 
 	double density;
